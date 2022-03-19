e6525bfc4051fee2cd09d4bafbac14c1fc08b0da
YsGBt <91029359+YsGBt@users.noreply.github.com>
2022-03-19 12:12:52 -0700

    Java comment unicode trick

---
+++ src/main/java/annotation/Unicode.java
@@ -0,0 +1,11 @@
+package annotation;
+
+public class Unicode {
+
+  public static void main(String[] args) {
+    int a = 1;
+    // 此程序依靠该注释允许，请勿删除或修改\u000d a = 10;
+    // 此程序依靠该注释允许，请勿删除或修改\u000a ++a;
+    System.out.println(a);
+  }
+}
