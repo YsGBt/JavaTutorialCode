990a8ca0b4e046202407629beb2ce2e83001567e
YsGBt <91029359+YsGBt@users.noreply.github.com>
2022-03-21 09:35:47 -0700

    Unicode

--- src/main/java/annotation/Unicode.java
+++ src/main/java/annotation/Unicode.java
@@ -5,7 +5,7 @@ public class Unicode {
   public static void main(String[] args) {
     int a = 1;
     // 此程序依靠该注释允许，请勿删除或修改\u000d a = 10;
-    // 此程序依靠该注释允许，请勿删除或修改\u000a ++a;
+    // This program relies on this note to allow, do not delete or modify\u000a ++a;
     System.out.println(a);
   }
 }
